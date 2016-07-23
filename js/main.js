1d0c889c42b305e87324e7352c032e77066f8976
Julio Aenlle <gmg8r251@ufl.edu>
2016-07-23 10:40:44 -0400

    Add comments to header jQuery in main js file

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
+    //this jQuery code adds a hover state to the icons in the contact section of the header
 
-    $('#email').hover(function () {
+    $('#email').hover(function () { //hover state for email icon
         $(this).attr('src', 'img/contact/envelope_hover.png');
     }, function () {
         $(this).attr('src', 'img/contact/envelope.png');
     });
 
-    $('#github').hover(function () {
+    $('#github').hover(function () { //hover state for GitHub icon
         $(this).attr('src', 'img/contact/github_hover.png');
     }, function () {
         $(this).attr('src', 'img/contact/github.png');
     });
 
-    $('#linkedin').hover(function () {
+    $('#linkedin').hover(function () { //hover state for Linkedin icon
         $(this).attr('src', 'img/contact/linkedin_hover.png');
     }, function () {
         $(this).attr('src', 'img/contact/linkedin.png');
