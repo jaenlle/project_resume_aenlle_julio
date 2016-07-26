5542991a2eca4824dc212bb70f092e365ff3eaf9
Julio Aenlle <gmg8r251@ufl.edu>
2016-07-26 16:00:31 -0400

    Modify index,css & js to add footer

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
     //this jQuery code adds a hover state to the icons in the contact section of the header
 
-    $('#email').hover(function () { //hover state for email icon
+    $('.email').hover(function () { //hover state for email icon
         $(this).attr('src', 'img/contact/envelope_hover.png');
     }, function () {
         $(this).attr('src', 'img/contact/envelope.png');
     });
 
-    $('#github').hover(function () { //hover state for GitHub icon
+    $('.github').hover(function () { //hover state for GitHub icon
         $(this).attr('src', 'img/contact/github_hover.png');
     }, function () {
         $(this).attr('src', 'img/contact/github.png');
     });
 
-    $('#linkedin').hover(function () { //hover state for Linkedin icon
+    $('.linkedin').hover(function () { //hover state for Linkedin icon
         $(this).attr('src', 'img/contact/linkedin_hover.png');
     }, function () {
         $(this).attr('src', 'img/contact/linkedin.png');
